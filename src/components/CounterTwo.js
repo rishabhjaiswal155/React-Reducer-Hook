43411e75bf12247718b06ce886ee0adf9fc16c1d
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-06-08 18:23:18 +0530

    Counter example with useReducer hook(Complex state and action as a object )

--- src/components/CounterTwo.js
+++ src/components/CounterTwo.js
@@ -25,13 +25,13 @@ function CounterTwo() {
         <div>CounterTwo<br />
             <div>firstCounter={count.firstCounter}</div>
             <div>secondCounter={count.secondCounter}</div>
-            <button onClick={() => dispatch({ type: 'increment', value: 1 })}>Increment</button>
-            <button onClick={() => dispatch({ type: 'decrement', value: 1 })}>Decrement</button>
-            <button onClick={() => dispatch({ type: 'increment', value: 5 })}>Increment 5</button>
-            <button onClick={() => dispatch({ type: 'decrement', value: 5 })}>Decrement 5</button>
+            <button onClick={() => dispatch({ type: 'increment', value: 1 })}>Increment counter 1</button>
+            <button onClick={() => dispatch({ type: 'decrement', value: 1 })}>Decrement counter 1</button>
+            <button onClick={() => dispatch({ type: 'increment', value: 5 })}>Increment counter 1 by 5</button>
+            <button onClick={() => dispatch({ type: 'decrement', value: 5 })}>Decrement counter 1 by 5</button>
             <div>
-            <button onClick={() => dispatch({ type: 'increment2', value: 1 })}>Increment</button>
-            <button onClick={() => dispatch({ type: 'decrement2', value: 1 })}>Decrement</button>
+            <button onClick={() => dispatch({ type: 'increment2', value: 1 })}>Increment counter 2</button>
+            <button onClick={() => dispatch({ type: 'decrement2', value: 1 })}>Decrement counter 2</button>
             </div>
             <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
         </div>
